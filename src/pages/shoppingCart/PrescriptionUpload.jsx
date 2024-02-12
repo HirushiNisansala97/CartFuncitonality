40877ab9406c6a564a4796e7effbf096ddc9cb70
HirushiNisansala97 <s18091-sci.pdn.ac.lk>
2024-02-12 08:43:31 +0530

    first commit

---
+++ src/pages/shoppingCart/PrescriptionUpload.jsx
@@ -0,0 +1,150 @@
+import React from "react";
+import '../shoppingCart/Shoppingcart.css';
+import  { useState, useEffect } from 'react';
+import '../../assets/css/style.css';
+
+const PrescriptionUpload =()=>{
+
+    
+
+    return(
+
+        <>
+            <div className="mainBody">
+                <div className="navBar">
+                        <div className="logo"></div>
+                        <div className="name"><b>UNITED PHARMA</b>
+                            <div className="healthShop">Health Shop</div>
+                        </div>
+                        <div className="slogan">
+                            <i>A Novel Experience on your Drug purchase</i>
+                        </div>
+                </div>
+                <div className="headerImg">
+                    <b class='shoppingPage'>Prescription Upload</b> 
+                </div>
+
+                    <form id="checkout-form" className="checkout-form wow fadeInUp delay-0-2s" name="checkout-form" action="#" method="post" style={{ visibility: 'visible' }}>
+                        <div className="row">
+                            <div className="rowOne">
+                                <div className="col-lg-12">
+                                    <h4>Patient Information</h4>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <input type="text" id="first-name" name="first-name" className="form-control" placeholder="First Name" value="" />
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <input type="text" id="last-name" name="last-name" className="form-control" placeholder="Last Name" value="" />
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <input type="text" id="number" name="number" className="form-control" placeholder="Phone Number" value="" />
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <input type="email" id="email" name="email" className="form-control" placeholder="Email Address" value="" />
+                                </div>
+                            </div>
+                            
+                            <div className="col-lg-12">
+                                <h4>Patient Address</h4>
+                            </div>
+                            {/* <div className="col-md-6 mb-30">
+                                <div className="form-group">
+                                    <select name="country" id="country" style={{ display: 'none' }}>
+                                        <option value="value1">Select Country</option>
+                                        <option value="value2">Australia</option>
+                                        <option value="value3">Canada</option>
+                                        <option value="value4">China</option>
+                                        <option value="value5">Morocco</option>
+                                        <option value="value6">Saudi Arabia</option>
+                                        <option value="value7">United Kingdom (UK)</option>
+                                        <option value="value8">United States (US)</option>
+                                    </select>
+                                    <div className="nice-select wide">
+                                        <ul className="list">
+                                            <li className="option">Select Country</li>
+                                            <li className="option">Australia</li>
+                                            <li className="option">Canada</li>
+                                            <li className="option">China</li>
+                                            <li className="option">Morocco</li>
+                                            <li className="option">Saudi Arabia</li>
+                                            <li className="option">United Kingdom (UK)</li>
+                                            <li className="option">United States (US)</li>
+                                        </ul>
+                                        <span className="current">Select Country</span>
+                                    </div>
+                                </div>
+                            </div> */}
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <input type="text" id="city" name="house_unit" className="form-control" placeholder="House/Appartment/Unit" value="" />
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <input type="text" id="state" name="street" className="form-control" placeholder="Street" value="" />
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <input type="text" id="zip" name="district" className="form-control" placeholder="District" value="" />
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <input type="text" id="street-name" name="province" className="form-control" placeholder="Province" value="" />
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="form-group">
+                                    <input type="text" id="apartment-name" name="zipCode" className="form-control" placeholder="Zip Code" value="" />
+                                </div>
+                            </div>
+                            <div className="col-lg-12">
+                                <h4>Attach Your Doctor's Prescription:</h4>
+                            </div>
+                            <div className="attachFile">
+                                <span className="wpcf7-form-control-wrap" data-name="pfile1">
+                                    <input
+                                        size="40"
+                                        className="wpcf7-form-control wpcf7-file wpcf7-validates-as-required"
+                                        accept=".pdf,.jpg,.jpeg"
+                                        aria-required="true"
+                                        aria-invalid="false"
+                                        type="file"
+                                        name="pfile1"
+                                    />
+                                </span>
+                            </div>
+                            <div className="col-lg-12">
+                                <h4>Order Notes (optional)</h4>
+                            </div>
+                            <div className="col-md-12">
+                                <div className="form-group mb-0">
+                                    <textarea name="order-note" id="order-note" className="form-control" rows="5" placeholder="Notes about your order, e.g. special notes for delivery."></textarea>
+                                </div>
+                            </div>
+
+                            <div className="submitBtn">
+                            <button class="theme-btn flex-none" type="submit">Submit <i class="fas fa-angle-double-right"></i></button>
+                            </div>
+                        </div>
+                    </form>
+            </div>
+            
+
+        
+        </>
+
+
+    )
+}
+export default PrescriptionUpload;
---
+++ src/pages/shoppingCart/ShoppingCart.jsx
@@ -0,0 +1,150 @@
+import React from "react";
+import '../shoppingCart/Shoppingcart.css';
+import  { useState, useEffect } from 'react';
+import '../../assets/css/style.css';
+
+
+const ShoppingCart =()=>{
+
+    
+
+    return(
+
+        <>
+        <div className="mainBody">
+            <div className="navBar">
+                <div className="logo"></div>
+                <div className="name"><b>UNITED PHARMA</b>
+                    <div className="healthShop">Health Shop</div>
+                </div>
+                <div className="slogan">
+                    <i>A Novel Experience on your Drug purchase</i>
+                </div>
+            </div>
+            <div className="headerImg">
+                 <b class='shoppingPage'>SHOPPING CART</b> 
+            </div>
+
+            <div className="cartTable">
+            <div className="cart-item-wrap mb-35 wow fadeInUp delay-0-2s" style={{ visibility: 'visible' }}>
+      <div className="cart-single-item">
+        <button type="button" className="close">
+          <span aria-hidden="true">×</span>
+        </button>
+        <div className="cart-img">
+          {/* <img src="assets/images/widgets/product2.png" alt="Product Image" /> */}
+        </div>
+        <h5 className="product-name">Orange Fruits</h5>
+        <span className="product-price">250</span>
+        <div className="quantity-input">
+          <button className="quantity-down">-</button>
+          <input className="quantity" type="text" name="quantity" value="1" />
+          <button className="quantity-up">+</button>
+        </div>
+        <span className="product-total-price">250</span>
+      </div>
+      <div className="cart-single-item">
+        <button type="button" className="close">
+          <span aria-hidden="true">×</span>
+        </button>
+        <div className="cart-img">
+          {/* <img src="assets/images/widgets/product3.png" alt="Product Image" /> */}
+        </div>
+        <h5 className="product-name">Fresh Broccoli</h5>
+        <span className="product-price">250</span>
+        <div className="quantity-input">
+          <button className="quantity-down">-</button>
+          <input className="quantity" type="text" name="quantity" value="1" />
+          <button className="quantity-up">+</button>
+        </div>
+        <span className="product-total-price">250</span>
+      </div>
+      <div className="cart-single-item">
+        <button type="button" className="close">
+          <span aria-hidden="true">×</span>
+        </button>
+        <div className="cart-img">
+        {/* <img src={require('../../assets/img/')} alt="bread"/> */}
+          {/* <img src="assets/images/" alt="Product Image" /> */}
+        </div>
+        <h5 className="product-name">Bread Grains</h5>
+        <span className="product-price">250</span>
+        <div className="quantity-input">
+          <button className="quantity-down">-</button>
+          <input className="quantity" type="text" name="quantity" value="1" />
+          <button className="quantity-up">+</button>
+        </div>
+        <span className="product-total-price">250</span>
+      </div>
+    </div>
+            </div>
+            <div className="btnLine">
+                <div className="btnAlign">
+                    
+                    <button class="theme-btn flex-none" type="submit">Coupon Code <i class="fas fa-angle-double-right"></i></button>
+                    <button class="theme-btn flex-none" type="submit">Apply Coupon <i class="fas fa-angle-double-right"></i></button>
+                    <button class="theme-btn flex-none" type="submit">Shopping <i class="fas fa-angle-double-right"></i></button>
+                    <button class="theme-btn flex-none" type="submit">Update Cart <i class="fas fa-angle-double-right"></i></button>
+                  
+                    {/* <button className="couponCode" type="submit"> Coupon Code <i class="fas fa-angle-double-right"></i></button>
+                
+                    <button className="applyCoupon " type="submit">Apply Coupon <i class="fas fa-angle-double-right"></i></button>
+                
+                    <button className="shopping " type="submit"> Shopping <i class="fas fa-angle-double-right"></i></button>
+                
+                    <button className="updateCart " type="submit">Update Cart <i class="fas fa-angle-double-right"></i></button> */}
+                    </div>
+            </div>
+
+            <div className="navBar">
+                
+                <div className="address"><b>Address</b>
+                    <div class="addressLines" data-pl="pl-address-detail">
+                        <div class="addressHead">
+                            <div className="phoneNumber">0715900395</div>
+                            <div class="shippingName">M.A.H. Nisansala</div>
+                        </div>
+                            <div class="addressContent large">195/d, alubovila, delgoda</div>
+                            <div class="pladdressContent large">Gampaha </div>
+                        </div>
+                        <div className="addressChangeBtn">
+                            <button class="theme-btn flex-none" type="submit">Change <i class="fas fa-angle-double-right"></i></button>
+                        </div>
+                </div>
+
+                <div className="cartTotal"><b>Cart Total</b>
+                    <table className="cartTotalTable">
+                        <tbody>
+                            <tr>
+                                <td>Cart Subtotal</td>
+                                <td class="sub-total-price">750.00</td>
+                            </tr>
+                            <tr>
+                                <td>Shipping Fee</td>
+                                <td class="shipping-price">10.0</td>
+                            </tr>
+                            <tr>
+                                <td>Vat</td>
+                                <td class="vat">112.50</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Order Total</strong></td>
+                                <td><strong class="total-price">882.50</strong></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div className="checkoutBtn">
+                         <button class="theme-btn flex-none" type="submit">Proceed to Checkout <i class="fas fa-angle-double-right"></i></button>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+
+        </>
+
+
+    )
+}
+export default ShoppingCart;
